3fc978441da36f8670b484923f0495cc6bf4d4b0
Rudy Hardeman <zarya@gigafreak.net>
2013-03-05 14:02:58 +0100

    some cleanup and more options

--- py/searcher.py
+++ py/searcher.py
@@ -3,11 +3,13 @@ import re,sys,os, urllib2, urllib, gzip, glob, json
 import ConfigParser
 import argparse
 import functions
+import time
 
 parser = argparse.ArgumentParser(description="Search the directory of shares")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-l", "--local", help="Search local", action='store_true')
 group.add_argument("-a", "--all", help="Search global", action='store_true')
+parser.add_argument("-f", "--file", help="Search files", action='store_true')
 parser.add_argument("-j", "--json", help="Output data in json format", action='store_true')
 parser.add_argument("-t", "--timestamp", help="Search from this timestamp", action='store_true')
 parser.add_argument("key", type=str, help="Keyword or Timestamp")
@@ -24,16 +26,12 @@ recursive = config.get('General', 'recursive')
 
 class result(object):
     def __init__(self, host=None, relpath=None, name=None, section=None, imdb=None, modified = None):
-        self.host = host
-        self.relpath = relpath
+        self.host = host.rstrip("\r\n")
+        self.relpath = relpath.rstrip("\r\n")
         self.name = name
         self.section = section
         self.imdb = imdb
         self.modified = modified
-
-if len(sys.argv) <= 2:
-    print 'No arguments?????\nHierr!11\n'
-    sys.exit(0)
 
 search = args.key 
 
@@ -57,9 +55,9 @@ for gz_file in gz_files:
     gz_uri = gz_sites[int(gz_filename.replace(".gz",""))]
 
     for line in gz:
-        if line.startswith("d"):
+        if (line.startswith("f") and args.file) or (line.startswith("d") and not args.file):
             relpath = " "
-            relpath = relpath.join(line.split(relpath)[4:]).replace('\n','')
+            relpath = relpath.join(line.split(relpath)[4:]).rstrip('\n\r')
             search = search.lower()
 
             if search in relpath.replace('_',' ').replace('-',' ').replace('.',' ').lower() and relpath.count('/') <= recursive:
@@ -80,13 +78,13 @@ if len(results) <= 0:
 data = []
 
 for result in results:
-    data.append({"host":result.host,"name":result.name,"section":result.section,"url":result.host + urllib.quote(result.relpath),"modified":result.modified})
+    data.append({"host":result.host,"name":result.name,"section":result.section,"url":result.host + urllib.quote(result.relpath),"modified":result.modified,"modified_nice":time.ctime(int(result.modified))})
 
 if len(data) > 1 and args.json:
     print json.dumps(data)
 
 if len(data) > 1 and not args.json:
-    titles = [('name',"Name"),('section',"Section"),('url',"URL"),('modified',"Date modified")]
+    titles = [('name',"Name"),('section',"Section"),('url',"URL"),('modified_nice',"Date modified")]
     functions.table_print(data, titles)
 
 sys.exit(1)
